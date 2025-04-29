154284dbbe00ed73697d585bdfe1ded2ea4783b7
MazenABMA <mazen.abma@gmail.com>
2025-04-29 20:41:10 +0300

    updated the login page

--- WebSecService/resources/views/users/login.blade.php
+++ WebSecService/resources/views/users/login.blade.php
@@ -28,22 +28,21 @@
         <input type="password" class="form-control" placeholder="password" name="password" required>
       </div>
 
-      {{-- Submit Button --}}
       <div class="form-group mb-2">
-        <button type="submit" class="btn btn-primary w-100">Login</button>
-      </div>
-      <div class="form-group mb-2 text-center d-flex justify-content-around">
-    {{-- Google Login Button --}}
-    <a href="{{ url('auth/google') }}" class="btn btn-danger" style="display: flex; align-items: center; gap: 5px;">
-        <img src="https://www.svgrepo.com/show/475656/google-color.svg" width="20" height="20" alt="Google">
-        <span style="font-size: 14px;">Login with Google</span>
-    </a>
+  <button type="submit" class="btn" style="background-color: black; color: white; width: 100%;">Login</button>
+</div>
 
-    {{-- Facebook Login Button --}}
-    <a href="{{ url('auth/facebook') }}" class="btn btn-primary" style="display: flex; align-items: center; gap: 5px;">
-        <img src="https://www.svgrepo.com/show/475645/facebook-color.svg" width="20" height="20" alt="Facebook">
-        <span style="font-size: 14px;">Login with Facebook</span>
-    </a>
+<div class="form-group mb-2 text-center d-flex justify-content-around">
+  {{-- Google Login Button --}}
+  <a href="{{ url('auth/google') }}" class="btn" style="background-color: white; color: #d9534f; border: 1px solid #d9534f; display: flex; align-items: center; gap: 5px;">
+    <span style="font-size: 14px;">Login with Google</span>
+  </a>
+
+  {{-- Facebook Login Button --}}
+  <a href="{{ url('auth/facebook') }}" class="btn btn-primary" style="display: flex; align-items: center; gap: 5px;">
+    <span style="font-size: 14px;">Login with Facebook</span>
+  </a>
+</div>
 </div>
  
 
